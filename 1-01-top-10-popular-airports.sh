b4bd842248999d85533a326db83bef8755add00d
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2016-02-12 14:03:28 +0000

    receivers

--- 1-01-top-10-popular-airports.sh
+++ 1-01-top-10-popular-airports.sh
@@ -5,4 +5,17 @@ THIS="$(basename $0)"
 THIS=${THIS/.sh}
 TOPIC=origin-destination
 
-spark-submit --master yarn --packages org.apache.spark:spark-streaming-kafka_2.10:1.5.2 --num-executors 6 --deploy-mode client $SCRIPT_DIR/${THIS}.py ip-172-30-0-47.ec2.internal:6667 $TOPIC
+spark-submit --packages org.apache.spark:spark-streaming-kafka_2.10:1.5.2 \
+  --master yarn \
+  --deploy-mode client \
+  --driver-memory 4g \
+  --executor-memory 4g \
+  --num-executors 24 \
+  --executor-cores 4 \
+  --conf spark.yarn.executor.memoryOverhead=400 \
+  --conf spark.streaming.backpressure.enabled=true \
+  --conf spark.streaming.kafka.maxRate=5000 \
+  $SCRIPT_DIR/${THIS}.py ip-172-30-0-239.ec2.internal:2181 $TOPIC
+  #$SCRIPT_DIR/${THIS}.py ip-172-30-0-47.ec2.internal:6667 $TOPIC
+
+  # This was for Direct Streaming
--- python/1-01-top-10-popular-airports.py
+++ python/1-01-top-10-popular-airports.py
@@ -11,14 +11,16 @@ if __name__ == "__main__":
         print("Usage: script.py <zk> <topic>", file=sys.stderr)
         exit(-1)
 
-    brokers, topic = sys.argv[1:]
+    zkQuorum, topic = sys.argv[1:]
+    #brokers, topic = sys.argv[1:]
 
     sc = SparkContext(appName="KafkaSparkStreaming")
     sc.setLogLevel("WARN")
-    context = StreamingContext(sc, 20)
-    context.checkpoint("checkpoint")
+    ssc = StreamingContext(sc, 5)
+    ssc.checkpoint("checkpoint")
 
-    ks = KafkaUtils.createDirectStream(context, [topic], {"metadata.broker.list": brokers})
+    #ks = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
+    ks = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 24})
 
     def producePerOriginOrDest(line):
         return ((airport, 1) for airport in line[1].split("\t")[1:2])
@@ -28,15 +30,41 @@ if __name__ == "__main__":
             runningCount = 0
         return sum(newValues, runningCount)
 
+    # class DictAccumulatorParam(AccumulatorParam):
+    #     def zero(self, init):
+    #         return {}
+
+    #     def addInPlace(self, v1, v2):
+    #         skey = str(v2[0])
+    #         val = int(v2[1])
+    #         oval = v1.get(skey, 0) + int(v2[1])
+    #         v1[skey] = oval
+    #         return v1
+
+    # accum = DictAccumulatorParam({})
+    # def addToPrevious(key, newValue):
+    #     accum.add([key, newValue])
+
+    counts = {}
+    def addToPrevious(key, newValue):
+        skey = str(key)
+        val = counts.get(skey, 0) + int(newValue)
+        counts[skey] = val
+        return val
+            #.updateStateByKey(updateFunction)\
+                #.map(lambda (x, y): (x, (addToPrevious(x, y))))\
+                        #.reduceByKey(lambda x, y: x + y)\
+
     digest = ks.flatMap(producePerOriginOrDest)\
-            .updateStateByKey(updateFunction)\
-            .transform(lambda rdd: rdd.sortBy(lambda x: x[1], ascending=False))
+               .updateStateByKey(updateFunction)\
+               .transform(lambda rdd: rdd.sortBy(lambda x: x[1], ascending=False))
 
     def toCSVLine(data):
         return ','.join('"' + str(d) + '"' for d in data)
     lines = digest.map(toCSVLine)
 
-    lines.pprint()
+    #digest.saveAsTextFiles("/user/ubuntu/testing/see")
+    digest.pprint()
 
-    context.start()
-    context.awaitTermination()
+    ssc.start()
+    ssc.awaitTermination()
