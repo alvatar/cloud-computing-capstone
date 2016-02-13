20c8e5dbae46bf61707bb681a191381489c48f33
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2016-02-13 10:28:53 +0000

    keep banging my head

--- 1-01-top-10-popular-airports.sh
+++ 1-01-top-10-popular-airports.sh
@@ -5,17 +5,21 @@ THIS="$(basename $0)"
 THIS=${THIS/.sh}
 TOPIC=origin-destination
 
-spark-submit --packages org.apache.spark:spark-streaming-kafka_2.10:1.5.2 \
+spark-submit --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 \
   --master yarn \
   --deploy-mode client \
-  --driver-memory 4g \
-  --executor-memory 4g \
-  --num-executors 24 \
+  --driver-memory 10g \
+  --executor-memory 10g \
+  --num-executors 6 \
   --executor-cores 4 \
-  --conf spark.yarn.executor.memoryOverhead=400 \
+  --conf spark.yarn.executor.memoryOverhead=1000 \
+  --conf spark.streaming.blockInterval=200ms \
   --conf spark.streaming.backpressure.enabled=true \
-  --conf spark.streaming.kafka.maxRate=5000 \
-  $SCRIPT_DIR/${THIS}.py ip-172-30-0-239.ec2.internal:2181 $TOPIC
+  --conf spark.streaming.kafka.maxRate=120000 \
+  --conf spark.streaming.receiver.maxRate=120000 \
+  $SCRIPT_DIR/${THIS}.py ip-172-30-0-211.ec2.internal:2181 $TOPIC
+
+  #$SCRIPT_DIR/${THIS}.py ip-172-30-0-239.ec2.internal:2181 $TOPIC
   #$SCRIPT_DIR/${THIS}.py ip-172-30-0-47.ec2.internal:6667 $TOPIC
 
   # This was for Direct Streaming
--- python/1-01-top-10-popular-airports.py
+++ python/1-01-top-10-popular-airports.py
@@ -12,53 +12,42 @@ if __name__ == "__main__":
         exit(-1)
 
     zkQuorum, topic = sys.argv[1:]
-    #brokers, topic = sys.argv[1:]
 
     sc = SparkContext(appName="KafkaSparkStreaming")
     sc.setLogLevel("WARN")
     ssc = StreamingContext(sc, 5)
     ssc.checkpoint("checkpoint")
 
-    #ks = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
-    ks = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 24})
+    # numStreams = 6
+    # kss = [KafkaUtils.createStream(ssc,\
+    #                                zkQuorum,\
+    #                                "spark-streaming-consumer",\
+    #                                {topic: 12})\
+    #                                for _ in range (numStreams)]
+    # ks = ssc.union(*kss)
+
+    #ks = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 12})
+
+    ks = ssc.socketTextStream("localhost", 9999)
 
     def producePerOriginOrDest(line):
-        return ((airport, 1) for airport in line[1].split("\t")[1:2])
+        year, origin, dest = line[1].split("\t")
+        return ((origin, 1), (dest, 1))
+
+    def processInput(line):
+        year, origin, dest = line.split("\t")
+        return ((origin, 1), (dest, 1))
 
     def updateFunction(newValues, runningCount):
         if runningCount is None:
             runningCount = 0
         return sum(newValues, runningCount)
 
-    # class DictAccumulatorParam(AccumulatorParam):
-    #     def zero(self, init):
-    #         return {}
-
-    #     def addInPlace(self, v1, v2):
-    #         skey = str(v2[0])
-    #         val = int(v2[1])
-    #         oval = v1.get(skey, 0) + int(v2[1])
-    #         v1[skey] = oval
-    #         return v1
-
-    # accum = DictAccumulatorParam({})
-    # def addToPrevious(key, newValue):
-    #     accum.add([key, newValue])
-
-    counts = {}
-    def addToPrevious(key, newValue):
-        skey = str(key)
-        val = counts.get(skey, 0) + int(newValue)
-        counts[skey] = val
-        return val
-            #.updateStateByKey(updateFunction)\
-                #.map(lambda (x, y): (x, (addToPrevious(x, y))))\
-                        #.reduceByKey(lambda x, y: x + y)\
-
-    digest = ks.flatMap(producePerOriginOrDest)\
+    digest = ks.flatMap(processInput)\
                .updateStateByKey(updateFunction)\
                .transform(lambda rdd: rdd.sortBy(lambda x: x[1], ascending=False))
 
+        
     def toCSVLine(data):
         return ','.join('"' + str(d) + '"' for d in data)
     lines = digest.map(toCSVLine)
