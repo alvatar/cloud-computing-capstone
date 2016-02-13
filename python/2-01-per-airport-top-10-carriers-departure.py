1b3727b9459878a8eff35571793ef1207af7f658
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2016-02-13 21:06:29 +0000

    testing Cassandra

--- python/2-01-per-airport-top-10-carriers-departure.py
+++ python/2-01-per-airport-top-10-carriers-departure.py
@@ -1,24 +1,30 @@
 from __future__ import print_function
 
 import sys
+import os
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
+
+from cassandra.cluster import Cluster
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: script.py <zk> <topic>", file=sys.stderr)
         exit(-1)
 
-    brokers, topic = sys.argv[1:]
+    zkQuorum, topic = sys.argv[1:]
 
     sc = SparkContext(appName="KafkaSparkStreaming")
     sc.setLogLevel("WARN")
-    context = StreamingContext(sc, 2)
-    context.checkpoint("checkpoint")
+    ssc = StreamingContext(sc, 10)
+    ssc.checkpoint("checkpoint")
 
-    ks = KafkaUtils.createDirectStream(context, [topic], {"metadata.broker.list": brokers})
+    ks = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 42})
+
+    cassandraCluster = Cluster(contact_points=[os.getenv('CASSANDRA', 'localhost')])
+    cassandra = cassandraCluster.connect('spark')
 
     def producePerFlight(line):
         val = line[1].split("\t")
@@ -26,9 +32,7 @@ if __name__ == "__main__":
         return((val[6], val[4]), float(val[8]))
 
     def movingAvg(newValues, movingAvg):
-        if movingAvg is None:
-            movingAvg = (0,0)
-        prevAvg, prevN = movingAvg
+        prevAvg, prevN = movingAvg or (0,0)
         currentN = len(newValues)
         return (float(prevAvg*prevN + sum(newValues)) / (prevN + currentN), prevN + currentN)
 
@@ -38,11 +42,17 @@ if __name__ == "__main__":
              .groupByKey()\
              .map(lambda x: (x[0], sorted(x[1], key=lambda val: val[1])[:10]))
 
+    cassandra.execute(
+        """
+        insert into test (key, val) values ('asdf', 'alvatar')
+        """
+    )
+
     def toCSVLine(data):
         return ','.join('"' + str(d) + '"' for d in data)
     lines = digest.map(toCSVLine)
 
-    lines.pprint(50)
+    lines.pprint()
 
-    context.start()
-    context.awaitTermination()
+    ssc.start()
+    ssc.awaitTermination()
