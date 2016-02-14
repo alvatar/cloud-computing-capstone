3ed7c86f1bb0b2fbda96edeea243fb3007412af6
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2016-02-14 17:29:52 +0000

    A kafkian movement

---
+++ kafka/create-topics.sh
@@ -0,0 +1,2 @@
+bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic ontime --partitions 42 --replication-factor 1
+bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic 2008ontime --partitions 42 --replication-factor 1
