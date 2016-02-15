f5012bbb81dbff6f78930c8b20dc8b343e9ebde4
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2016-02-15 01:36:19 +0000

    Kafkian scripts

--- kafka/delete-topics.sh
+++ kafka/delete-topics.sh
@@ -1,2 +1,2 @@
-bin/kafka-topics.sh --zookeeper localhost:2181 --delete --topic ontime
-bin/kafka-topics.sh --zookeeper localhost:2181 --delete --topic 2008ontime
+$KAFKA_HOME/bin/kafka-topics.sh --zookeeper localhost:2181 --delete --topic ontime
+$KAFKA_HOME/bin/kafka-topics.sh --zookeeper localhost:2181 --delete --topic 2008ontime
